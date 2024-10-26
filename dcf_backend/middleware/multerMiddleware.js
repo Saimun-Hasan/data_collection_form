65265cb833f150db54dfac5c3c65809b808a0c2c
Saimun-Hasan <shasan@reviewthatplace.com>
2024-10-26 14:26:51 +0600

    updated updated multer

--- dcf_backend/middleware/multerMiddleware.js
+++ dcf_backend/middleware/multerMiddleware.js
@@ -1,6 +1,10 @@
+// multerMiddleware.js
 import multer from 'multer';
 
+const storage = multer.memoryStorage();
+
 const upload = multer({
+    storage,
     limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(mp3|wav)$/)) {
